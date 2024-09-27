ca9686193f9699c73435b2ad60ef58f267f932e8
thisioliverthings <Abodeking19999@gmail.com>
2024-09-27 21:37:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,34 @@
 const { Telegraf } = require('telegraf');
-const Jimp = require('jimp');
 
-const bot = new Telegraf('7739626112:AAHVJXMdorsiiyTsp9wtclsbnks84m4g8eI');
+// استبدل '8119443898:AAFwm5E368v-Ov-M_XGBQYCJxj1vMDQbv-0' بالتوكن الخاص بك
+const bot = new Telegraf('YOUR_BOT_API_KEY');
 
-bot.on('photo', async (ctx) => {
-    const photo = ctx.message.photo[ctx.message.photo.length - 1];
-    const fileId = photo.file_id;
-    const fileLink = await ctx.telegram.getFileLink(fileId);
-
-    console.log(fileLink); // طباعة الرابط للتأكد
-
-    try {
-        const image = await Jimp.read(fileLink);
-        // معالجة الصورة هنا
-    } catch (error) {
-        console.error('Error processing image:', error);
-        ctx.reply('حدث خطأ أثناء معالجة الصورة.');
-    }
+// معالج الأمر /start
+bot.start((ctx) => {
+    ctx.reply('أهلا بك في البوت! كيف يمكنني مساعدتك؟');
 });
 
-bot.launch();
+// معالج الرسائل النصية
+bot.on('text', (ctx) => {
+    console.log('Received a message:', ctx.message.text); // طباعة الرسالة المستلمة في السجل
+    ctx.reply('شكرا على رسالتك!'); // رد على المستخدم
+});
+
+// معالج الصور
+bot.on('photo', async (ctx) => {
+    const photo = ctx.message.photo[ctx.message.photo.length - 1]; // الحصول على أكبر صورة
+    const fileId = photo.file_id; // معرف الصورة
+    const fileLink = await ctx.telegram.getFileLink(fileId); // الحصول على رابط الصورة
+
+    console.log('Received photo link:', fileLink); // طباعة رابط الصورة
+    ctx.reply('لقد تلقيت صورة!'); // رد على المستخدم
+});
+
+// تشغيل البوت
+bot.launch()
+    .then(() => {
+        console.log('بوت Telegram يعمل...');
+    })
+    .catch((err) => {
+        console.error('حدث خطأ أثناء تشغيل البوت:', err);
+    });
